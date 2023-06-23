f34a87c24ec29d056e4bdd51e145702082db57b5
konkatenazia <daniilsahnov@mail.ru>
2023-06-23 23:57:14 +0300

    oooo

--- src/main/java/com/kankaBot/kankaBot/SpringDemoBotApplication.java
+++ src/main/java/com/kankaBot/kankaBot/SpringDemoBotApplication.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringDemoBotApplication {
-
-//	public static void main(String[] args) {
-//		SpringApplication.run(SpringDemoBotApplication.class, args);
-//	}
-
 	public static void main(String[] args) {
 		try {
 			SpringApplication.run(SpringDemoBotApplication.class, args);
